e460675f57490adb6857da2bf9e66584dfb665c9
Lakshmi Murugappan <lakshmimuru.art@gmail.com>
2024-05-02 11:41:41 -0400

    creating slurm file for submitting jobs on cluster

---
+++ prover/links.bash
@@ -0,0 +1,7 @@
+curl -L -o prover-bank-task1.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/prover/entailmentbank_task1/stepwise/epoch%3D499-step%3D10500.ckpt
+curl -L -o prover-bank-task1-single.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/prover/entailmentbank_task1/single_shot/epoch%3D399-step%3D8400.ckpt
+curl -L -o prover-bank-task2.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/prover/entailmentbank_task2/stepwise/epoch%3D599-step%3D12600.ckpt
+curl -L -o prover-bank-task2-single.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/prover/entailmentbank_task2/single_shot/epoch%3D399-step%3D8400.ckpt
+curl -L -o verifier-bank-task1.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/verifier/entailmentbank_task1/epoch%3D49-step%3D36300.ckpt
+curl -L -o verifier-bank-task2.ckpt https://huggingface.co/kaiyuy/NLProofS/resolve/main/verifier/entailmentbank_task2/epoch%3D49-step%3D36300.ckpt
+
